845bf76392a3e7fc3fa629c0869c112439930de2
saberla <1850065377@qq.com>
2019-06-18 15:54:55 +0800

    update

--- 新建文件夹/js/location.js
+++
@@ -1,9 +0,0 @@
-/**
- * Created by Thinkpad on 2019/1/12.
- */
-function screenLocation(){
-    var htLocation = document.getElementById("container");
-    var screenWidth =  window.screen.width - 17;
-    htLocation.style.cssText = "width:" + screenWidth + "px";
-    console.log(screenWidth);
-}
